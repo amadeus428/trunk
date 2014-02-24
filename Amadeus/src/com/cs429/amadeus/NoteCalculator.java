50b1f0a0ffeecb705b1efe1006af6ce0bcf9b878
mythichelm <mjhymel111@gmail.com>
2014-02-23 20:23:46 -0600

    pithadi2/hymel2 forgot to include NoteCalculator skeleton class

---
+++ Amadeus/src/com/cs429/amadeus/NoteCalculator.java
@@ -0,0 +1,17 @@
+package com.cs429.amadeus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NoteCalculator {
+	
+	
+	public NoteCalculator(){
+		
+	}
+
+	
+	public String calculateNote(double frequency) {
+		return null;
+	}
+}
