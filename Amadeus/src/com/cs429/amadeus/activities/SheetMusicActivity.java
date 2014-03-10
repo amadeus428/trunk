583639a4d1c623203bb30a1053d377da3612acbc
mythichelm <mjhymel111@gmail.com>
2014-03-09 19:37:58 -0500

    merged stephen/nick's sheet music code

--- Amadeus/src/com/cs429/amadeus/activities/SheetMusicActivity.java
+++ Amadeus/src/com/cs429/amadeus/activities/SheetMusicActivity.java
@@ -1,15 +1,19 @@
 package com.cs429.amadeus.activities;
 
 import com.cs429.amadeus.R;
+import com.cs429.amadeus.StaffView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class SheetMusicActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sheet_music);
+		setContentView(R.layout.staff_example);
+		FrameLayout frame = (FrameLayout) findViewById(R.id.frame_view);
+		frame.addView(new StaffView(this));
 	}
 }
