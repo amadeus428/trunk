dabe520db1099050eb5cd75f6e88ac34ed137d8c
Jonathan <jonlowe21@gmail.com>
2014-03-30 20:46:41 -0500

    Added chord tabs and renamed chord image files

--- Amadeus/src/com/cs429/amadeus/fragments/GuitarChordFragment.java
+++ Amadeus/src/com/cs429/amadeus/fragments/GuitarChordFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,7 +16,7 @@ import android.widget.Spinner;
 import com.cs429.amadeus.R;
 import com.cs429.amadeus.activities.MainActivity;
 
-public class GuitarChordFragment extends Fragment {
+public class GuitarChordFragment extends Fragment implements OnItemSelectedListener{
 
 	public GuitarChordFragment()
 	{
@@ -47,41 +48,68 @@ public class GuitarChordFragment extends Fragment {
 		getActivity().setTitle("Chords");
 
 		//Do other set up things here, like set up listeners
-		Spinner dropdown = (Spinner)getActivity().findViewById(R.id.spinner1);
-		String[] items = new String[]{"A", "C", "D", "E", "F", "G"};
+		Spinner dropdown = (Spinner)getActivity().findViewById(R.id.fragment_guitar_chords_chord_spinner);
+		String[] items = new String[]{"A", "A2", "A4", "Am", "C", "D", "D2", "D4", "Dm", "D/F#", "E", "E4", "F", "G", "G/B"};
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, items);
 		dropdown.setAdapter(adapter);
-		//dropdown.setOnItemSelectedListener(this.getActivity());
+		dropdown.setOnItemSelectedListener(this);
 	}
 	
+	@Override
 	public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-
+		ImageView image = (ImageView) getActivity().findViewById(R.id.fragment_guitar_chords_chord_image);
         switch (position) {
             case 0:
-                // What ever you want to happen when item 1 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Achord);
+            	image.setImageResource(R.drawable.a);
                 break;
             case 1:
-                // What ever you want to happen when item 2 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Cchord);
+            	image.setImageResource(R.drawable.a2);
                 break;
             case 2:
-                // What ever you want to happen when item 3 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Dchord);
+            	image.setImageResource(R.drawable.a4);
                 break;
             case 3:
-                // What ever you want to happen when item 3 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Echord);
+            	image.setImageResource(R.drawable.aminor);
                 break;
             case 4:
-                // What ever you want to happen when item 3 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Fchord);
+            	image.setImageResource(R.drawable.c);
                 break;
             case 5:
-                // What ever you want to happen when item 3 selected
-            	//ImageView image = (ImageView) getActivity().findViewById(R.id.Gchord);
+            	image.setImageResource(R.drawable.d);
                 break;
-            
+            case 6:
+            	image.setImageResource(R.drawable.d2);
+                break;
+            case 7:
+            	image.setImageResource(R.drawable.d4);
+                break;
+            case 8:
+            	image.setImageResource(R.drawable.dminor);
+                break;
+            case 9:
+            	image.setImageResource(R.drawable.dslashfsharp);
+                break;
+            case 10:
+            	image.setImageResource(R.drawable.e);
+                break;
+            case 11:
+            	image.setImageResource(R.drawable.e4);
+                break;
+            case 12:
+            	image.setImageResource(R.drawable.f);
+                break;
+            case 13:
+            	image.setImageResource(R.drawable.g);
+                break;
+            case 14:
+            	image.setImageResource(R.drawable.gslashb);
+                break;
         }
     }
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
