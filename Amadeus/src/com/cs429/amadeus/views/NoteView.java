6100b005486e2f91080a596059a46ae4517e611a
Nicolas Patenode <nicolas@nicolas-V5-171.(none)>
2014-04-14 14:25:11 -0500

    Added highlighting and tests

--- Amadeus/src/com/cs429/amadeus/views/NoteView.java
+++ Amadeus/src/com/cs429/amadeus/views/NoteView.java
@@ -21,14 +21,18 @@ public class NoteView extends View {
     private Note note;
     private Bitmap bitmap;
     private Rect transformation;
+    private Rect highlightRect;
     private Rect sharpTransformation;
-
+    
+    
     public NoteView(Context context, StaffLayout parent, Note note) {
 	super(context);
 
 	this.parent = parent;
 	this.note = note;
 	this.bitmap = StaffLayout.getBitmap(note.type);
+	
+	makeHighlightBitmaps(bitmap);
 
 	int width = parent.getNoteWidth();
 	int height = parent.getNoteHeight();
@@ -42,6 +46,13 @@ public class NoteView extends View {
 	} else {
 	    transformation = new Rect(0, 0, width, height);
 	}
+	
+	
+	//TODO This code is for highlighting notes vs filling them in
+	//highlightRect = new Rect((int)(transformation.left * .8),(int)( transformation.top *.8),(int) (transformation.right *1.1),(int)( transformation.bottom*1.1));
+    highlightRect = new Rect((int)(transformation.left ),(int)( transformation.top),(int) (transformation.right),(int)( transformation.bottom));
+    transformation.inset((int)((transformation.right-transformation.left)*.05), (int)((transformation.bottom-transformation.top)*.05));
+   	transformation.offset((int)(transformation.right-transformation.left*.05), (int)(transformation.bottom-transformation.top*.05));
     }
 
     @Override
@@ -63,6 +74,14 @@ public class NoteView extends View {
 	if (note.isSharp) {
 	    canvas.drawBitmap(StaffLayout.sharpBitmap, null, sharpTransformation, null);
 	}
+	
+	//If this note should be highlighted for feedback to the user for play-along
+	if(this.highlight == Highlight.Green)
+		canvas.drawBitmap(greenBitmap[this.note.type], null, highlightRect, null);
+	else if(this.highlight == Highlight.Red)
+		canvas.drawBitmap(redBitmap[this.note.type], null, highlightRect, null);
+
+	
 	canvas.drawBitmap(bitmap, null, transformation, null);
 
 	super.onDraw(canvas);
@@ -71,4 +90,47 @@ public class NoteView extends View {
     public Note getNote() {
 	return note;
     }
+    /*
+     * This method is used to generate green and red bitmaps for notes
+     * 
+     * It is called every time a note object is made, but it only makes bitmaps if colored bitmaps
+     * have note already been made for this note type (As-needed generation)
+     */
+    private void makeHighlightBitmaps(Bitmap bitmap){
+    	
+    	if(greenBitmap[this.note.type] != null){ //A colored bitmap has already been generated for this note type 
+    		return;
+    	}
+    	
+    	greenBitmap[this.note.type] = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+    	redBitmap[this.note.type] = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+
+    	for(int i = 0; i < bitmap.getWidth(); i++ ){
+    		for(int j = 0; j < bitmap.getHeight(); j++){
+    			int pixel = bitmap.getPixel(i, j);
+    			redBitmap[this.note.type].setPixel(i, j, pixel | (511 << 16));
+    			greenBitmap[this.note.type].setPixel(i, j, pixel | (511 << 8));
+    		}
+    	}
+    }
+    static private Bitmap [] greenBitmap = new Bitmap[5];
+    static private Bitmap [] redBitmap = new Bitmap[5];  
+    
+    public static Bitmap [] getGreenBitmaps(){
+    	return greenBitmap;
+    }
+    public static Bitmap [] getRedBitmaps(){
+    	return redBitmap;
+    }
+    public Bitmap getBitmap(){
+    	return bitmap;
+    }
+    protected enum Highlight{
+    	Red,
+    	Green,
+    	None
+    }
+    
+    
+    private Highlight highlight = Highlight.None;
 }
---
+++ Amadeus_Test/src/com/cs429/amadeus/test/junit/NoteViewTest.java
@@ -0,0 +1,69 @@
+package com.cs429.amadeus.test.junit;
+
+import junit.framework.TestCase;
+import android.graphics.Bitmap;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.cs429.amadeus.Note;
+import com.cs429.amadeus.activities.MainActivity;
+import com.cs429.amadeus.views.NoteView;
+import com.cs429.amadeus.views.StaffLayout;
+
+public class NoteViewTest extends ActivityInstrumentationTestCase2<MainActivity>  {
+	public MainActivity mActivity;
+	
+	StaffLayout mStaff;
+	
+	public NoteViewTest() {
+		super(MainActivity.class);
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		setActivityInitialTouchMode(false);
+
+		mActivity = getActivity();
+		
+	}
+
+	public void testGreenBitmap(){
+		mStaff = new StaffLayout(mActivity);
+		
+		Note note = new Note('A', 5, false, Note.QUARTER_NOTE);
+		
+		NoteView noteView = new NoteView(mActivity, mStaff, note); 
+		
+		Bitmap [] greenBitmap = NoteView.getGreenBitmaps();
+		Bitmap noteBitmap = noteView.getBitmap();
+		
+		for(int i = 0; i< noteBitmap.getWidth(); i++){
+			for(int j = 0; j< noteBitmap.getHeight(); j++){
+				if(noteBitmap.getPixel(i, j) == 0x0){
+					continue;
+				}
+				assertTrue((noteBitmap.getPixel(i, j) & (511 << 8)) < (greenBitmap[note.type].getPixel(i, j) & (511 << 8)));
+			}
+		}
+	}
+	public void testRedBitmap(){
+		mStaff = new StaffLayout(mActivity);
+		
+		Note note = new Note('A', 5, false, Note.QUARTER_NOTE);
+		
+		NoteView noteView = new NoteView(mActivity, mStaff, note); 
+		
+		Bitmap [] redBitmap = NoteView.getRedBitmaps();
+		Bitmap noteBitmap = noteView.getBitmap();
+		
+		for(int i = 0; i< noteBitmap.getWidth(); i++){
+			for(int j = 0; j< noteBitmap.getHeight(); j++){
+				if(noteBitmap.getPixel(i, j) == 0x0){
+					continue;
+				}
+				assertTrue((noteBitmap.getPixel(i, j) & (511 << 16)) < (redBitmap[note.type].getPixel(i, j) & (511 << 16)));
+			}
+		}
+	}
+}
