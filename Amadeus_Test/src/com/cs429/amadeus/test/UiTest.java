4b94381e0ec64ca7bee182ecc2f5772bdc579e7e
Hirizzle <hpithadia92@gmail.com>
2014-04-27 21:14:33 -0500

    Refactoring UITEST

--- Amadeus_Test/src/com/cs429/amadeus/test/UiTest.java
+++ Amadeus_Test/src/com/cs429/amadeus/test/UiTest.java
@@ -56,6 +56,77 @@ public class UiTest extends ActivityInstrumentationTestCase2<MainActivity> {
 		mImage = (ImageView) mActivity
 				.findViewById(R.id.fragment_guitar_chords_chord_image);
 		Assert.assertNotNull(mImage);
+		
+		mSpinner = (Spinner) mActivity
+				.findViewById(R.id.fragment_guitar_chords_chord_spinner);
+		mActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				mSpinner.requestFocus();
+				mSpinner.setSelection(0);
+			}
+		});
+
+		// check to see all spinner values are correct.
+		for (int i = 0; i < 15; i++) {
+			this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+			mImage = (ImageView) mActivity
+					.findViewById(R.id.fragment_guitar_chords_chord_image);
+			int pos = mSpinner.getSelectedItemPosition();
+				
+			int id = (Integer) mImage.getTag();
+			switch (pos) {
+				case 0:
+					assertEquals(id,R.drawable.a);
+					break;
+				case 1:
+					assertEquals(id,R.drawable.a2);
+					break;
+				case 2:
+					assertEquals(id,R.drawable.a4);
+					break;
+				case 3:
+					assertEquals(id,R.drawable.aminor);
+					break;
+				case 4:
+					assertEquals(id,R.drawable.c);
+					break;
+				case 5:
+					assertEquals(id,R.drawable.d);
+					break;
+				case 6:
+					assertEquals(id,R.drawable.d2);
+					break;
+				case 7:
+					assertEquals(id,R.drawable.d4);
+					break;
+				case 8:
+					assertEquals(id,R.drawable.dminor);
+					break;
+				case 9:
+					assertEquals(id,R.drawable.dslashfsharp);
+					break;
+				case 10:
+					assertEquals(id,R.drawable.e);
+					break;
+				case 11:
+					assertEquals(id,R.drawable.e4);
+					break;
+				case 12:
+					assertEquals(id,R.drawable.eminor);
+					break;
+				case 13:
+					assertEquals(id,R.drawable.f);
+					break;
+				case 14:
+					assertEquals(id,R.drawable.g);
+					break;
+				case 15:
+					assertEquals(id,R.drawable.gslashb);
+					break;
+			}
+			this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+
+		}
 
 		selectDrawer(R.string.title_guitar_tabs);
 		WebView mWeb = ((WebView) getActivity().findViewById(R.id.tab_text));
@@ -65,6 +136,9 @@ public class UiTest extends ActivityInstrumentationTestCase2<MainActivity> {
 		Button button = (Button) mActivity
 				.findViewById(R.id.fragment_home_play_along_button);
 		assertNotNull(button);
+		
+		selectDrawer(R.string.title_sound_profile);
+		Button button
 
 	}
 
@@ -111,42 +185,6 @@ public class UiTest extends ActivityInstrumentationTestCase2<MainActivity> {
 		Assert.assertNotNull(mStaffLayout);
 	}
 
-	/*public void testGuitar() {
-
-		mActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				((Button) mActivity
-						.findViewById(R.id.fragment_home_guitar_chords_button))
-						.performClick();
-			}
-		});
-		mInstrumentation.waitForIdleSync();
-		mImage = (ImageView) mActivity
-				.findViewById(R.id.fragment_guitar_chords_chord_image);
-		Assert.assertNotNull(mImage);
-
-		mSpinner = (Spinner) mActivity
-				.findViewById(R.id.fragment_guitar_chords_chord_spinner);
-		mActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				mSpinner.requestFocus();
-				mSpinner.setSelection(0);
-			}
-		});
-
-		// check to see all spinner values are correct.
-		for (int i = 0; i < 15; i++) {
-			this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
-			mImage = (ImageView) mActivity
-					.findViewById(R.id.fragment_guitar_chords_chord_image);
-			int pos = mSpinner.getSelectedItemPosition();
-			int id = (Integer) mImage.getTag();
-			assertEquals(id, R.drawable.a + pos);
-			System.out.println(id);
-			this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
-
-		}
-	}*/
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
